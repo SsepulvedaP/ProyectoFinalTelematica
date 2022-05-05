4ae960d5a9c3a7ceec131d998b3c09e84c4df919
Tumaco <103197008+Tumaco@users.noreply.github.com>
2022-05-05 17:48:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 #Se llaman las librerias
 from dash import Dash, html, dcc #Esta es la libreria para el servidor web del data science
 import pandas as pd              #Esta libreria es para procesar los datos
+import plotly.graph_objs as go
 
 #Declarar objetos principales
 app = Dash(__name__)
 #Cargar la base de datos
 df = pd.read_excel('datanoticias.xlsx')
 #Configuracion del sitio web
-app.layout = html.Div([html.H1('Bienvenido a la seccion de noticias Barquituma'),
-                       html.Div('Noticia 1:')
-                       html.Div('Noticia 2:')])
+app.layout = html.Div([html.H1(df['Bienvenido a la seccion de noticias Barquituma']),
+                       html.Div(df['Noticia 1:'])
+                       html.Div(df['Noticia 2:'])])
 #funcion principal
 if __name__ == '__main__':
   #Cargar el objeto principal a todas las interfaces de red en el puerto 80
